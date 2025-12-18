c92eecf660364d6e217888b6ad46c1b2637c7301
krpcn1r <zaystep09@gmail.com>
2025-12-18 10:16:57 +0300

    Update routes.py

--- app/task_1/routes.py
+++ app/task_1/routes.py
@@ -1,46 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-
-route_instance = None
-
-
-class Routes:
-    def __init__(self, name, num, text):
-        self.name = name
-        self.num = num
-        self.text = text
 
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return "Привет"
 
 
 @app.route("/hello/<name>")
 def hello_user(name):
-    if route_instance and route_instance.name:
-        return f"Привет {route_instance.name}"
-    return f"Привет {name}"
+    return f"Привет: {name}"
 
 
 @app.route("/square/<int:number>")
 def num_square(number):
-    return str(number * number)
+    return f"Квадрат числа {number}: {str(number * number)}"
 
 
-@app.route("/text")
-def show_text():
-    if route_instance:
-        return f"Текст из конструктора: {route_instance.text}"
-    return "Данные не инициализированы"
+@app.route("/length/<text>")
+def text_length(text):
+    return f"Длина строки '{text}': {len(text)}"
 
 
-if __name__ == '__main__':
-    name = input("Введите имя: ")
-    num = int(input("Введите число: "))
-    text = input("Введите строку: ")
-
-    route_instance = Routes(name, num, text)
-
-    app.run(debug=True)
+@app.route("/reverse", methods=['POST'])
+def reverse_text():
+    data = request.data.decode('utf-8')
+    return data[::-1]
